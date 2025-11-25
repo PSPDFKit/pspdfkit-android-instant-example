1e08b7f46b240952761d29ea4ba05e97158e28ce
Nutrient <>
2025-11-25 09:25:23 +0000

    Update example to Nutrient for Android 10.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         minSdk = 26
         targetSdk = compileSdk
 
-        versionName = "10.8.0"
-        versionCode = 144807
+        versionName = "10.9.0"
+        versionCode = 145049
 
         resValue("string", "NUTRIENT_LICENSE_KEY", "\"LICENSE_KEY_GOES_HERE\"")
     }
@@ -39,7 +39,7 @@ android {
 dependencies {
     // Nutrient is integrated from the Nutrient Maven repository. See the `repositories` block at the beginning
     // of this file, which shows how to set up the repository in your app.
-    implementation("io.nutrient:nutrient:10.8.0")
+    implementation("io.nutrient:nutrient:10.9.0")
 
     // Retrofit and Gson for web example REST API.
     implementation("com.google.code.gson:gson:2.10.1")
